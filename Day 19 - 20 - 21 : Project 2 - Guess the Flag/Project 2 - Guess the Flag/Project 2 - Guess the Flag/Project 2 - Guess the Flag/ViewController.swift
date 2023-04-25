13a94b576a3e5c8d0dd507c82ee4a74e750c124b
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-04-25 12:01:37 -0300

    Add files via upload

--- Day 19 - 20 - 21 : Project 2 - Guess the Flag/Project 2 - Guess the Flag/Project 2 - Guess the Flag/Project 2 - Guess the Flag/ViewController.swift
+++ Day 19 - 20 - 21 : Project 2 - Guess the Flag/Project 2 - Guess the Flag/Project 2 - Guess the Flag/Project 2 - Guess the Flag/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     
     override func viewDidLoad() {
@@ -37,11 +38,35 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
  
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
 }
 
