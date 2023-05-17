2a42eab49726abf0e1e195ffe155f095f34f55f9
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-05-17 11:15:55 -0300

    Add files via upload

---
+++ Day 33,34,35 - PROJECT 7 /Project 7/Project 7/Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by Lucas Neves dos santos pompeu on 16/05/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Day 33,34,35 - PROJECT 7 /Project 7/Project 7/Project 7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by Lucas Neves dos santos pompeu on 16/05/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
