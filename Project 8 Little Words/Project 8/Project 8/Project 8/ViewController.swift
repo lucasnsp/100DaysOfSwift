6d54d61725712df38ff95839b5119019063a0b22
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-05-22 16:33:46 -0300

    Add files via upload

--- Project 8 Little Words/Project 8/Project 8/Project 8/ViewController.swift
+++ Project 8 Little Words/Project 8/Project 8/Project 8/ViewController.swift
@@ -17,7 +17,11 @@ class ViewController: UIViewController {
     var activatedButtons = [UIButton]()
     var solutions = [String]()
     
-    var score = 0
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     var level = 1
     
     override func loadView() {
@@ -139,16 +143,56 @@ class ViewController: UIViewController {
     
     
     @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
         
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
     }
 
     
     @objc func submitTapped(_ sender: UIButton) {
+        guard let answerText = currentAnswer.text else { return }
+
+            if let solutionPosition = solutions.firstIndex(of: answerText) {
+                activatedButtons.removeAll()
+
+                var splitAnswers = answersLabel.text?.components(separatedBy: "\n")
+                splitAnswers?[solutionPosition] = answerText
+                answersLabel.text = splitAnswers?.joined(separator: "\n")
+
+                currentAnswer.text = ""
+                score += 1
+
+                if score % 7 == 0 {
+                    let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
+                    present(ac, animated: true)
+                }
+            }
+    }
+    
+    func levelUp(action: UIAlertAction) {
+        level += 1
         
+        solutions.removeAll(keepingCapacity: true)
+        loadLevel()
+        
+        for button in letterButtons {
+            button.isHidden = false
+        }
     }
     
     
     @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
+        
+        for button in activatedButtons {
+            button.isHidden = false
+            
+        }
+        
+        activatedButtons.removeAll()
         
     }
     
