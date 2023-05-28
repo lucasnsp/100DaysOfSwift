92c99861d40fb10eec72e9c13c5782198b87f09f
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-05-27 22:18:40 -0300

    Add files via upload

---
+++ Project 10/Project 10/Project 10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Lucas Neves dos santos pompeu on 27/05/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    
+}
---
+++ Project 10/Project 10/Project 10/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project 10
+//
+//  Created by Lucas Neves dos santos pompeu on 27/05/23.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else{
+            fatalError("Unable to dequeue PersonCell")
+        }
+        return cell
+    }
+
+}
+
