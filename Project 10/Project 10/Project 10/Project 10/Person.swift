511872daaa7847ca5f89b1c54d9d86221b333e62
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-05-29 11:47:31 -0300

    Add files via upload

---
+++ Project 10/Project 10/Project 10/Project 10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Lucas Neves dos santos pompeu on 29/05/23.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
