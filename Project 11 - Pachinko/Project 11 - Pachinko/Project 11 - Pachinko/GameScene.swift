ab5bd6ab250c01eb6139ab56af0664b46a229bba
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-05-31 13:10:44 -0300

    Add files via upload

---
+++ Project 11 - Pachinko/Project 11 - Pachinko/Project 11 - Pachinko/GameScene.swift
@@ -0,0 +1,45 @@
+//
+//  GameScene.swift
+//  Project 11 - Pachinko
+//
+//  Created by Lucas Neves dos santos pompeu on 31/05/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeBoucer(at: CGPoint(x: 0, y: 20))
+        makeBoucer(at: CGPoint(x: 256, y: 20))
+        makeBoucer(at: CGPoint(x: 512, y: 20))
+        makeBoucer(at: CGPoint(x: 768, y: 20))
+        makeBoucer(at: CGPoint(x: 1024, y: 20))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+    
+    func makeBoucer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+}
