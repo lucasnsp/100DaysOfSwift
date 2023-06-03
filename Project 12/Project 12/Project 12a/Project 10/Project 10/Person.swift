7da1c4ec6b4b79e442654942eb29e7ab143aeffa
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-06-03 11:46:45 -0300

    Add files via upload

---
+++ Project 12/Project 12/Project 12a/Project 10/Project 10/Person.swift
@@ -0,0 +1,28 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Lucas Neves dos santos pompeu on 29/05/23.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+}
