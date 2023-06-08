a1151dca4040af5df905394687d0184358fcfc0f
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-06-08 11:49:25 -0300

    Add files via upload

---
+++ Project 13/Project 13/Project 13/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Project 13
+//
+//  Created by Lucas Neves dos santos pompeu on 08/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+
+    @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "InstaFilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+}
+
