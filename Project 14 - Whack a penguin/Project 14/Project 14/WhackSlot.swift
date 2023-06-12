1fa669f7b67b87861fd2aeae570f85056871bd20
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-06-12 17:51:56 -0300

    Add files via upload

---
+++ Project 14 - Whack a penguin/Project 14/Project 14/WhackSlot.swift
@@ -0,0 +1,64 @@
+//
+//  WhackSlot.swift
+//  Project 14
+//
+//  Created by Lucas Neves dos santos pompeu on 12/06/23.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
+    
+    var isVisible = false
+    var isHit = false
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+    
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+            
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.5))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [ weak self ] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+}
