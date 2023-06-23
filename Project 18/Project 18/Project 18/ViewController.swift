46e6e67c45824e2bc2508f28d15eb6392d82dffb
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-06-23 11:18:09 -0300

    Add files via upload

---
+++ Project 18/Project 18/Project 18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project 18
+//
+//  Created by Lucas Neves dos santos pompeu on 23/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
