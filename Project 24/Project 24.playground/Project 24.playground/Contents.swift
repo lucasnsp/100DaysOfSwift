ef15193ffb1a29ce0d0c00d56cefab3ad49babea
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-07-14 11:14:43 -0300

    Add files via upload

---
+++ Project 24/Project 24.playground/Project 24.playground/Contents.swift
@@ -0,0 +1,146 @@
+import UIKit
+
+//let name = "Taylor"
+//
+//for letter in name {
+//    print("Give me a \(letter)!")
+//}
+//
+//let letter = name[name.index(name.startIndex, offsetBy: 3)]
+//
+//
+//extension String {
+//    subscript(i: Int) -> String {
+//        return String(self[index(startIndex, offsetBy: i)])
+//    }
+//}
+//
+//
+//let letter2 = name[3]
+
+
+//let password = "12345"
+//password.hasPrefix("123")
+//password.hasSuffix("456")
+//
+//
+//extension String {
+//    func deletingPrefix(_ prefix: String) -> String {
+//        guard self.hasPrefix(prefix) else { return self }
+//        return String(self.dropFirst(prefix.count))
+//    }
+//
+//    func deletingSuffix(_ suffix: String) -> String {
+//        guard self.hasSuffix(suffix) else { return self }
+//        return String(self.dropLast(suffix.count))
+//    }
+//}
+
+//let weather = "it's going to rain"
+//print(weather.capitalized)
+//
+//extension String {
+//    var capitalizedFirst: String {
+//        guard let firstLetter = self.first else { return "" }
+//        return firstLetter.uppercased() + self.dropFirst()
+//    }
+//}
+
+
+//let input = "Swift is like Objective-C without the C"
+//input.contains("Swift")
+//
+//
+//let languages = ["Python", "Ruby", "Swift"]
+//languages.contains("Swift")
+//
+//
+//extension String {
+//    func containsAny(of array: [String]) -> Bool {
+//        for item in array {
+//            if self.contains(item) {
+//                return true
+//            }
+//        }
+//
+//        return false
+//    }
+//}
+//
+//input.containsAny(of: languages)
+//
+//
+//languages.contains(where: input.contains)
+
+
+//let string = "This is a test string"
+//
+//let attributes: [NSAttributedString.Key: Any] = [
+//    .foregroundColor: UIColor.white,
+//    .backgroundColor: UIColor.red,
+//    .font: UIFont.boldSystemFont(ofSize: 36)
+//]
+//
+//let attributedString = NSAttributedString(string: string, attributes: attributes)
+
+
+//let string = "This is a test string"
+//
+//let attributedString = NSMutableAttributedString(string: string)
+//
+//attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+//
+//attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+//
+//attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+//
+//attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+//
+//attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+
+
+// Challenge
+//One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try your new knowledge to make sure you fully understand what’s going on:
+//
+//1 - Create a String extension that adds a withPrefix() method. If the string already contains the prefix it should return itself; if it doesn’t contain the prefix, it should return itself with the prefix added. For example: "pet".withPrefix("car") should return “carpet”.
+//2 - Create a String extension that adds an isNumeric property that returns true if the string holds any sort of number. Tip: creating a Double from a String is a failable initializer.
+//3 - Create a String extension that adds a lines property that returns an array of all the lines in a string. So, “this\nis\na\ntest” should return an array with four elements.
+
+
+// challenge 1
+
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        guard !self.hasPrefix(prefix) else { return self }
+        return prefix + self
+    }
+}
+
+assert("test".withPrefix("te") == "test")
+assert("pet".withPrefix("car") == "carpet")
+
+
+// challenge 2
+
+extension String {
+    var isNumeric: Bool {
+        return Double(self) != nil
+    }
+}
+
+
+assert("test".isNumeric == false)
+assert("123".isNumeric == true)
+assert("456.7".isNumeric == true)
+
+
+// challenge 3
+
+extension String {
+    var lines: [Substring] {
+        return self.split(separator: "\n")
+    }
+}
+
+assert("this\nis\na\ntest".lines == ["this", "is", "a", "test"])
